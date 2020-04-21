cd78994ced08c811a0f04e6167a9aa0e38f097cf
Hannselthill Camacho <45092062+Hannsel101@users.noreply.github.com>
2020-04-21 09:29:24 -0700

    Created Module RegisterFile

---
+++ RegisterFile.v
@@ -0,0 +1,8 @@
+module RegisterFile(
+	input [2:0] R1, R2, R3,//Register pointers
+	input [12:0] ALU_Result,//Result from the ALU arithmetic operation
+	input WriteFlag);//Flag to enable a write into a register
+	
+	
+	
+endmodule
